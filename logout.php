ec4c00a2df9bb852d625a9510c5a675918fce364
Christian Wegerhoff <wegerhoff@mediaworx.com>
2019-10-22 11:53:34 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -9,6 +9,7 @@
   <script>
       document.cookie = "access=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Access Cookie löschen
       document.cookie = "refresh=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Refresh Cookie löschen
+      document.cookie = "vehicle=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Vehicle Toke Cookie löschen
       document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Legacy Tesla Toke Cookie löschen
       console.log("Tesla Token Cookies entfernt");
   </script>
