6d0bd48aac7887a72809991703654eb746d92f89
Christian Wegerhoff <wegerhoff@mediaworx.com>
2019-10-22 11:47:24 +0200

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8" />
+		<title>Tesla Logout</title>
+  <?php ?>
+  <script>
+      document.cookie = "access=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Access Cookie löschen
+
+      document.cookie = "refresh=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Refresh Cookie löschen
+      document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Legacy Tesla Toke Cookie löschen
+  </script>
+	</head>
+	<body>
+		<p id="done">Tesla Token entfernt. Logout erfolgreich.</p>
+	</body>
+</html>
