28a3fe86aff3fd2bc7eb00144d28f02608892cfd
Christian Wegerhoff <wegerhoff@mediaworx.com>
2019-10-27 10:36:22 +0100

    Last changes

--- logout.php
+++ logout.php
@@ -13,10 +13,10 @@
       try {revokeTeslaToken()}
       catch {console.log('Revoking token not successful')};
 
-      // document.cookie = "access=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Access Cookie löschen
-      // document.cookie = "refresh=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Refresh Cookie löschen
-      // document.cookie = "vehicle=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Legacy Tesla Toke Cookie löschen
-      // document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Legacy Tesla Toke Cookie löschen
+      document.cookie = "access=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Access Cookie löschen
+      document.cookie = "refresh=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Refresh Cookie löschen
+      document.cookie = "vehicle=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Legacy Tesla Toke Cookie löschen
+      document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //  Legacy Tesla Toke Cookie löschen
       console.log("Tesla Token Cookies entfernt");
 
       function getCookie(name) {
@@ -35,7 +35,6 @@
         });
 
         var xhr = new XMLHttpRequest();
-        // xhr.withCredentials = true;
 
         xhr.addEventListener("readystatechange", function () {
           if (this.readyState === 4) {
@@ -50,9 +49,7 @@
         xhr.setRequestHeader("cache-control", "no-cache");
 
         xhr.send(body);
-
       };
-
   </script>
 	</head>
 	<body>
