0292951853d73ed5c9f423d0b622c3b4b14c59f4
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-03 16:43:11 +0100

    Update test.php

--- test.php
+++ test.php
@@ -8,6 +8,7 @@ offset: +1 GMT
 */
 
 echo date("D M d Y"). ', current time : ' .time();
-echo date("D M d Y"). ', sunset time : ' .date_sunset(time()-10*24 * 60 * 60, SUNFUNCS_RET_STRING, 52.52, 13.41, 90, 1);
+echo '<br>';
+echo date("D M d Y"). ', sunset time : ' .date_sunset(time()-10*24 * 60 * 60, SUNFUNCS_RET_TIMESTAMP, 52.52, 13.41, 90, 1);
 
 ?>
