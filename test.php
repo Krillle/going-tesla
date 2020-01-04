31d1dfe26fe2d500effbea73d7371af2f9d16013
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-04 16:15:08 +0100

    Update test.php

--- test.php
+++ test.php
@@ -7,12 +7,15 @@ Zenith ~= 90
 offset: +1 GMT
 */
 
-echo date("D M d Y"). ', current time : ' .time();
+echo date("D M d Y"). ', current time : ' .time("H i");
 echo '<br>';
 echo date("D M d Y"). ', sunset time : ' .date_sunset(time(), SUNFUNCS_RET_TIMESTAMP, 52.52, 13.41, 90, 1);
 echo '<br>';
 echo '--<br>';
+echo time("H i");
+echo '<br>';
 echo date("H i");
+echo '<br>';
 echo gmdate("H i");
 echo '<br>---';
 ?>
