dae77a2ea76fb1d2ed0af2c71f6472733db31af0
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-06 20:43:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -881,7 +881,7 @@ if (isset($_GET["dark"])) {$darkmode = true;};
        var routeUrl = 'https://api.mapbox.com/directions/v5/mapbox/driving/'
           + start.longitude + ',' + start.latitude + ';'
           + destination.lng + ',' + destination.lat
-          + '?access_token=' + mapboxgl.accessToken + route ? '&geometries=polyline&overview=simplified' : '&overview=false'
+          + '?access_token=' + mapboxgl.accessToken + (route ? '&geometries=polyline&overview=simplified' : '&overview=false')
       result = httpGet(routeUrl)
       console.log("Result" + result);
       if (result) {
