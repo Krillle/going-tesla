b56e74f7bb2757769ab0659cb2d9442a39263ada
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-08 00:02:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -904,9 +904,6 @@ if (isset($_GET["dark"])) {$darkmode = true;};
     };
 
     function showRoute(coordinates) {
-      console.log('printing line', coordinates);
-      map.removeLayer('route');
-      map.removeSource('route');
       map.addLayer({
         'id': 'route',
         'type': 'line',
