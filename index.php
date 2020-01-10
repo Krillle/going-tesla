839a0c836c7da52b740e6ab4a042aa8b75a18c3c
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-10 16:41:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -347,7 +347,7 @@ if (isset($_GET["dark"])) {$darkmode = true;};
       showRoute(route.coordinates);
       var routeChargers = getRouteChargers(route.coordinates);
       var routeChargerList = '';
-      routeChargers.features.forEach( chargeLocation => {
+      routeChargers.features.forEach( chargeLocation.properties => {
         console.log("Chargelocation Listelement:", chargeLocation);
 
         routeChargerList += '<strong>${chargeLocation.name} ${chargeLocation.city}</strong><br';
@@ -1229,13 +1229,13 @@ if (isset($_GET["dark"])) {$darkmode = true;};
       };
       chargerList.chargelocations.forEach(chargeLocation => {
         newList.features.push(chargeLocationDetails(chargeLocation));
-        console.log('IN:',chargeLocation);
-        console.log('OUT;',chargeLocationDetails(chargeLocation));
       });
       map.getSource('chargers').setData(newList);
     };
 
     function chargerShortDescription (chargeLocation) {
+      console.log('IN:',chargeLocation);
+      
       var address = `${chargeLocation.street}, ${chargeLocation.city}, ${chargeLocation.country}`;
 
       var description = '';
