d5de79376fc47de116313e3e854b747092a01bb2
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-12 17:15:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -378,8 +378,8 @@ if (isset($_GET["dark"])) {$darkmode = true;};
       routeChargers.features.forEach( chargeLocation => {
         // routeChargerList += '<table border="0" width="100%"><tr><td align="left">Datum</td><td align="center">Text</td><td align="right">Quelle</td></tr></table>';
         routeChargerList += `<a href="#" onclick="flyToCharger(${chargeLocation.properties.coordinates.lng},${chargeLocation.properties.coordinates.lat},'${chargeLocation.properties.name}','${chargeLocation.properties.city}'); return false;">`;
-        routeChargerList += `<p><table border="0" width="100%"><tr><td align="left"><strong>${chargeLocation.properties.distance}</strong></td><td align="center"><strong>${chargeLocation.properties.duration}</strong></td>`;
-        routeChargerList += `<td align="right"><strong>${chargeLocation.properties.range ? chargeLocation.properties.range : ""}</strong></td></tr></table>`;
+        routeChargerList += `<p><table border="0" width="100%" style="border-collapse: collapse;"><tr><td align="left" style="padding: 0px;margin: 0px;"><strong>${chargeLocation.properties.distance}</strong></td><td align="center" style="padding: 0px;margin: 0px;"><strong>${chargeLocation.properties.duration}</strong></td>`;
+        routeChargerList += `<td align="right" style="padding: 0px;margin: 0px;"><strong>${chargeLocation.properties.range ? chargeLocation.properties.range : ""}</strong></td></tr></table>`;
         routeChargerList += `${chargeLocation.properties.name} ${chargeLocation.properties.name.includes(chargeLocation.properties.city) ? '' : chargeLocation.properties.city}<br>`;
         routeChargerList += `${chargeLocation.properties.count}x ${chargeLocation.properties.power} kW ${chargeLocation.properties.type}</p></a>`;
       });
@@ -1220,7 +1220,6 @@ if (isset($_GET["dark"])) {$darkmode = true;};
 
       if (includeDistance) {
         var route = getRoute(teslaPosition,{'longitude' : chargeLocation.coordinates.lng, 'latitude' : chargeLocation.coordinates.lat});
-        console.log('Range:', teslaPosition.range, route.distanceRaw,  teslaPosition.range - route.distanceRaw);
       };
       return {
         "id": chargeLocation.ge_id.toString(),
