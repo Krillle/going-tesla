3f7a914925f86a2da7a76014c5041150bacb5f8d
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-14 23:55:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -898,8 +898,8 @@ if (isset($_GET["dark"])) {$darkmode = true;};
         routeChargerList += `${chargeLocation.properties.count}x ${chargeLocation.properties.power} kW ${chargeLocation.properties.type}</p>`;
         routeChargerList += `</div></a>`;
       });
-      routeChargerList += `<div class="onecolumn"><a class="popupbutton" href="#" style="width: 292px;" onclick="cancelRouteChargerList(); return false;">Abbrechen</a>`;
-      routeChargerList += `<a class="popupbutton popupbutton-icon-highwayCharger" style="margin-left: 8px; width: 60px;" href="#" onclick="toggleeRouteList(); return false;"></a></div>`;
+      routeChargerList += `<div class="onecolumn"><a class="popupbutton" href="#" style="width: 275px;" onclick="cancelRouteChargerList(); return false;">Abbrechen</a>`;
+      routeChargerList += `<a class="popupbutton popupbutton-icon-highwayCharger" style="margin-left: 25px; width: 60px;" href="#" onclick="toggleeRouteList(); return false;"></a></div>`;
       routeList(routeChargerList);
 
       var route = getRoute(teslaPosition,{'longitude' : destination.center[0], 'latitude' : destination.center[1]},'full');
