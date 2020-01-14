4045900454ece506972490259e6507deddd02b0c
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-14 17:56:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -757,7 +757,7 @@ if (isset($_GET["dark"])) {$darkmode = true;};
     function getEncodedCookie(name) {
       var cookie = getCookie(name);
       console.log(cookie);
-      return (!typeof (cookie  === 'undefined') ? decodeURIComponent(cookie) : false;
+      return (!typeof (cookie  === 'undefined')) ? decodeURIComponent(cookie) : false;
     };
 
     function settingsPopup () {
