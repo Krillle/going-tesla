0ae9d75b15fcc32090111275817722a510216085
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-14 18:17:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -755,7 +755,11 @@ console.log(getCookie('destination'));
     function getCookie(name) {
       var value = "; " + document.cookie;
       var parts = value.split("; " + name + "=");
-      if (parts.length == 2) return parts.pop().split(";").shift();
+      if (parts.length == 2) {
+        return parts.pop().split(";").shift()
+      } else {
+        return false
+      };
     };
 
     function settingsPopup () {
