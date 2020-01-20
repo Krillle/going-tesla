54d35cdeaa21826e93b19d1d2de460b5169ef331
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-20 21:57:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -343,7 +343,7 @@ if (isset($_GET["dark"])) {$darkmode = true;};
 
     var teslaConnection = {'accessToken': getCookie('access'),'refreshToken': getCookie('refresh'), 'vehicle': getCookie('vehicle'), 'status': 'undefined' };
     // var teslaPosition = {'longitude' : 10.416667, 'latitude' : 51.133333, 'heading': 0, 'speed' : 100, 'zoom': 9, 'range': 0};
-    var teslaPosition = {'longitude' : 13.48, 'latitude' : 52.49, 'heading': 0, 'speed' : 100, 'zoom': 9, 'range': 64};
+    var teslaPosition = {'longitude' : 13.48, 'latitude' : 52.49, 'heading': 0, 'speed' : 100, 'zoom': 9, 'range': false};
 
     var currentDestination = JSON.parse(decodeURIComponent(getCookie('destination')));
 
