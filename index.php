d8aabca9d433d0c5802e27966bbd3d7e212bf7e5
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-01-25 23:02:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -150,8 +150,8 @@ if (isset($_GET["dark"])) {$darkmode = true;};
     .mapboxgl-ctrl-geocoder--icon-loading {
       width: 36px;
       height: 36px;
-      margin-top: 11px;
-      margin-left: 360px;
+      margin-top: 8x;
+      margin-left: 35px;
     }
 
     .mapboxgl-popup-anchor-bottom > .mapboxgl-popup-tip {
