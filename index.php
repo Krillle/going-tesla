29db4145d3ac8dff34e6eacc51716c70790986f5
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-02-16 02:08:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -963,8 +963,9 @@
           };
 
           setTeslaPosition(vehicleData.response);
-          if (positionIcon.geometry.coordinates != [teslaPosition.longitude,teslaPosition.latitude]
-              || positionIcon.properties.bearing != teslaPosition.heading) {
+          if (positionIcon.geometry.coordinates[0] != teslaPosition.longitude ||
+              positionIcon.geometry.coordinates[1] != teslaPosition.latitude ||
+              positionIcon.properties.bearing != teslaPosition.heading) {
 
 console.log('Delta:', positionIcon.geometry.coordinates,[teslaPosition.longitude,teslaPosition.latitude]);
 console.log('Bearing:', positionIcon.properties.bearing, teslaPosition.heading);
