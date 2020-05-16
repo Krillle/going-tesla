0e87f518be4bf914accb3fe20b42551f85f681ff
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-05-16 02:34:04 +0200

    Tauschen longitude

--- index.php
+++ index.php
@@ -942,16 +942,16 @@
 
     function encodeHash() {
       var center = map.getCenter();
-      return center.lng + ',' + center.lat + ',' + map.getZoom() + ',' + map.getBearing();
+      return center.lat + ',' + center.lng + ',' + map.getZoom() + ',' + map.getBearing();
     };
 
     function decodeHash(hash) {
       var payload = hash.substring(1).split(',');
 
-      console.log('Hash:' + ' longitude ' + payload[0]+ ', latitude ' + payload[1]+ ', zoom ' + payload[2] +', heading '+ payload[3]);
+      console.log('Hash:' + ' latitude ' + payload[0]+ ', longitude ' + payload[1]+ ', zoom ' + payload[2] +', heading '+ payload[3]);
 
-      if (payload[0]) {teslaPosition.longitude =  Number(payload[0]);console.log('longitude'+ Number(payload[0]));};
-      if (payload[1]) {teslaPosition.latitude =  Number(payload[1]);console.log('latitude'+ Number(payload[1]));};
+      if (payload[0]) {teslaPosition.latitude =  Number(payload[0]);console.log('longitude'+ Number(payload[0]));};
+      if (payload[1]) {teslaPosition.longitude =  Number(payload[1]);console.log('latitude'+ Number(payload[1]));};
       if (payload[2]) {teslaPosition.zoom =  Number(payload[2]);console.log('zoom'+ Number(payload[2]));};
       if (payload[3]) {teslaPosition.heading =  Number(payload[3]);console.log('heading' + Number(payload[3]));};
 
