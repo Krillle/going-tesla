24943348df66a663aeb68401d29bcdaf41b701f2
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-09-17 09:42:27 +0200

    <?php echo getenv('goingelectric'); ?>

---
+++ phpversion.php
@@ -0,0 +1,12 @@
+<<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    PHP $_Server:
+    <?php echo $_SERVER; ?>
+    GoingElectric Token: <?php echo getenv('goingelectric'); ?>
+  </body>
+</html>
