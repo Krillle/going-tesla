4c4f6e2b9c6b2db6ebe88135d922f6c517ae01a1
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-09-17 10:18:49 +0200

    phpinfo();

--- phpversion.php
+++ phpversion.php
@@ -5,10 +5,6 @@
     <title></title>
   </head>
   <body>
-    PHP $_Server: <?php print_r($_SERVER); ?><p>
-    GoingElectric Token: <?php echo getenv('goingelectric'); ?>
-
-
     <?php echo phpinfo(); ?> 
   </body>
 </html>
