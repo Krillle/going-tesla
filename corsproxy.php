e53c843ff9700fb5b54b7556b15d3c7fa016332e
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-09-18 09:21:12 +0200

    Remove tranformation

--- corsproxy.php
+++ corsproxy.php
@@ -62,16 +62,25 @@ $curl_options = array(
 
 /* * * STOP EDITING HERE UNLESS YOU KNOW WHAT YOU ARE DOING * * */
 
+// // identify request headers
+// $request_headers = array( );
+// foreach ($_SERVER as $key => $value) {
+//     csajax_debug_message('Header ' . $key . ' - ' . $value);
+//     if (strpos($key, 'HTTP_') === 0  ||  strpos($key, 'CONTENT_') === 0) {
+//         $headername = str_replace('_', ' ', str_replace('HTTP_', '', $key));
+//         $headername = str_replace(' ', '-', ucwords(strtolower($headername)));
+//         if (!in_array($headername, array( 'Host', 'X-Proxy-Url' ))) {
+//             $request_headers[] = "$headername: $value";
+//         }
+//     }
+// }
+
 // identify request headers
 $request_headers = array( );
-foreach (getallheaders() as $key => $value) {
+foreach (getallheaders() as $headername => $value) {
     csajax_debug_message('Header ' . $key . ' - ' . $value);
-    if (strpos($key, 'HTTP_') === 0  ||  strpos($key, 'CONTENT_') === 0) {
-        $headername = str_replace('_', ' ', str_replace('HTTP_', '', $key));
-        $headername = str_replace(' ', '-', ucwords(strtolower($headername)));
-        if (!in_array($headername, array( 'Host', 'X-Proxy-Url' ))) {
-            $request_headers[] = "$headername: $value";
-        }
+    if (!in_array($headername, array( 'Host', 'X-Proxy-Url' ))) {
+        $request_headers[] = "$headername: $value";
     }
 }
 
