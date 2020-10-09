2529aa33027c277d1a1e90a81c6c4245f383b6db
Christian Wegerhoff <wegerhoff@mediaworx.com>
2020-10-09 20:55:59 +0200

    test

--- index.php
+++ index.php
@@ -784,8 +784,18 @@
         "paint": {
           "line-offset": 5,
           "line-width": 2,
+          "line-opacity": 0,
           "line-color": [
             "case",
+            [
+              "==",
+              "low",
+              [
+                "get",
+                "congestion"
+              ]
+            ],
+            "#aab7ef",
             [
               "==",
               "moderate",
