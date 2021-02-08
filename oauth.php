ab4020538fd33f57e8888b4f309e854cb8c32c86
Christian Wegerhoff <wegerhoff@mediaworx.com>
2021-02-08 14:29:17 +0100

    done?

--- oauth.php
+++ oauth.php
@@ -517,6 +517,6 @@ $t->setClientId(trim($id[1]));
 $t->setClientSecret(trim($secret[1]));
 
 $body = json_decode(file_get_contents('php://input'), true);
-var_dump($t->getAccessToken($body['email'], $body['password']));
+print(json_encode($t->getAccessToken($body['email'], $body['password'])));
 
 ?>
