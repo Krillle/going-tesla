494481e23eeab8cdeb9eadd73491ae7a08049a2a
Christian Wegerhoff <wegerhoff@mediaworx.com>
2021-04-08 19:04:01 +0200

    full conext

--- refresh.php
+++ refresh.php
@@ -3,10 +3,17 @@
 include "lib/teslapi.php";
 
 $t = new Tesla();
-$body = json_decode(file_get_contents('php://input'), true);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://pastebin.com/raw/pS7Z6yyP');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+$api = explode(PHP_EOL,$result);
+$id=explode('=',$api[0]);
+$secret=explode('=',$api[1]);
+$t->setClientId(trim($id[1]));
+$t->setClientSecret(trim($secret[1]));
 
-echo "Body:" . $body;
-echo "Refresh" . $body['refreshtoken'];
+$body = json_decode(file_get_contents('php://input'), true);
 
 print(json_encode($t->refreshAccessToken($body['refreshtoken'])));
 
